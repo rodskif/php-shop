ef310b496fb3de4a8f790dda50490388f3ebd0f4
Yura Melnik <rodskif@ukr.net>
2018-02-14 16:55:46 +0200

    header done (1.6)

--- include/header.php
+++ include/header.php
@@ -1,5 +1,45 @@
-<div id="block-header">
-    <div id="header-top-block"></div>
-
-
+<div id="block-header" class="clearfix">
+    <div id="header-top-block">
+      <ul id="header-top-menu">
+        <li>Ваш город - <span>Киев</span></li>
+        <li><a href="about.php">О нас</a></li>
+        <li><a href="shops.php">Магазины</a></li>
+        <li><a href="contacts.php">Контакты</a></li>
+      </ul>
+      <div id="reg-auth-title">
+        <button class="top-auth">Вход</button>
+        <a href="registration.php">Регистрация</a>
+      </div>
+    </div>
+    
+    <hr class="line">
+    
+    <div class="logo">
+      <img class="img-logo" src="img/logo.png" alt="">
+      <div class="personal-info">
+       
+        <p class="personal-phone">Звонок бесплатный <br> 8 (800) 100-12-34</p>
+        <p class="personal-time">Режим работы <br> Буднии дни с 9:00 до 18:00 <br> Суббота, Воскресенье - выходные</p>
+        
+      </div>
+    </div>
+    <div id="block-search">
+      <form action="search.php?=q" method="get">
+        <span></span> 
+        <input type="text" id="input-search" name="q" placeholder="Поиск среди более 100 000 товаров">
+        <input type="submit" id="button-search" value="Поиск">
+      </form>
+    </div>
 </div>
+
+
+<div id="top-menu">
+  <ul>
+    <li><img src="img/shop.png" alt=""><a href="index.php">Главная</a></li>
+    <li><img src="img/new.png" alt=""><a href="#">Новинки</a></li>
+    <li><img src="img/bestprice.png" alt=""><a href="#">Лидеры продаж</a></li>
+    <li><img src="img/sale.png" alt=""><a href="#">Распродажа</a></li>
+    <li class="cart"><img src="img/cart.png" alt=""><a href="#">Корзина пуста</a></li>
+  </ul>
+</div>
+<hr class="line">
