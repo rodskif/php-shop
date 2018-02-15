81aaf4a51ad24aef22b73176fd154239af19f328
Yura Melnik <rodskif@ukr.net>
2018-02-15 21:45:13 +0200

    footer - start

--- include/footer.php
+++ include/footer.php
@@ -1,1 +1,41 @@
-<div id="block-footer"></div>
+<div id="block-footer">
+  <div id="footer-suport">
+    <p class="suport-item">Режим работы</p>
+    <p class="phone-item">8 (800) 100-12-34</p>
+    <p class="time-item">Режим работы: <br> Буднии дни с 9:00 до 18:00 <br> Суббота, Воскресенье - выходные</p>
+  </div>
+  <div id="footer-service">
+    <ul>
+      <li class="footer-title"><a href="#">Сервис и помощь</a></li>
+      <li><a href="#">Как сделать заказ</a></li>
+      <li><a href="#">Способы оплаты</a></li>
+      <li><a href="#">Возврат</a></li>
+      <li><a href="#">Публичная оферта</a></li>
+    </ul>
+  </div>
+  <div id="footer-company">
+    <ul>
+      <li class="footer-title"><a href="#">О компании</a></li>
+      <li><a href="#">О нас</a></li>
+      <li><a href="#">Вакансии</a></li>
+      <li><a href="#">Партнерам</a></li>
+      <li><a href="#">Контакты</a></li>
+    </ul>
+  </div>
+  <div id="footer-navigation">
+    <ul>
+      <li class="footer-title"><a href="#">Навигация</a></li>
+      <li><a href="index.php">Главная страница</a></li>
+      <li><a href="#">Обратная связь</a></li>
+    </ul>
+  </div>
+  <div id="footer-social">
+    <ul>
+      <li class="footer-title"><a href="#">Рассказать о сайте</a></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</div>
